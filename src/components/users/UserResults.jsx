4eb840b33f501bac9691c6e8d08ee9a7bbd80820
Alkibijad <aleksandar04081@gmail.com>
2022-07-21 15:42:46 -0700

    About page CSS

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,26 +1,40 @@
-import React,  { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function UserResults() {
-  const [users, setUsers] = useState();
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
   const fetchUsers = async () => {
-    const response = await fetch(
-      `${process.env.REACT_APP_GITHUB_URL}/users`,
-      {
-        headers: {
-          Authorization: `token${process.env.REACT_APP_GITHUB_TOKEN}`,
-        },
-      }
-    );
-      const data = await response.json();
-      console.log(data);
-  };
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    });
+    const data = await response.json();
+    setUsers(data);
+    setLoading(false);
+    };
+    
+    const showUsers = () => { 
+     return  users.map((user, index) => (
+            <h3 key={index}>{user.login}</h3>
+          ))
+    }
+    const showLoading = () => { 
+        return <p>Loading.....</p>
+    }
 
-  return <div>UserResults</div>;
+  return (
+      <div>
+          
+          {  users ? showUsers(): showLoading()}
+      
+    </div>
+  );
 }
 
 export default UserResults;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,27 +1,16 @@
 function About() {
   return (
-    <>
+    <div className="about">
       <h1>Github Finder</h1>
-      <p>
-        A React app to search GitHub profiles and see profile details. This
-        project is part of the
+      <p>A React app to search GitHub profiles and see profile details. This project is part of the
         <a href="https://www.udemy.com/course/modern-react-front-to-back/">
           React Front To Back
-        </a>
-        Udemy course by
-        <strong>
-          <a href="https://traversymedia.com"> Brad Traversy</a>
-        </strong>
-        .
+        </a>Udemy course by<strong><a href="https://traversymedia.com"> Brad Traversy</a></strong>.
       </p>
-      <p>
-        Version <span>1.0.0</span>
+      <p>Version <span>1.0.0</span></p>
+      <p>CSS design:<a href="https://github.com/Alkibijad?tab=repositories">Aleksandar "Sasha"</a>
       </p>
-      <p>
-        Layout By:
-        <a href="https://twitter.com/hassibmoddasser">Hassib Moddasser</a>
-      </p>
-    </>
+    </div>
   );
 }
 

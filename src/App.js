e030272201918768ec9a41bb30fa8a14e0b3c5a8
Alkibijad <aleksandar04081@gmail.com>
2022-07-26 13:23:06 -0700

    commi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import { GithubProvider } from "./context/github/GitHubContext";
 
 //import pages
 import Home from "./pages/Home";
@@ -10,17 +11,19 @@ import NotFound from "./pages/NotFound";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={ <Home/> }></Route>
-            <Route path="/About" element={ <About/> }></Route>
-            <Route path="/*" element={ <NotFound/> }></Route>
-          </Routes>
-        </main>
-      </Router>
-      <Footer/>
+      <GithubProvider>
+        <Router>
+          <Navbar />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/About" element={<About />}></Route>
+              <Route path="/*" element={<NotFound />}></Route>
+            </Routes>
+          </main>
+        </Router>
+      </GithubProvider>
+      <Footer />
     </div>
   );
 }
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,35 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useContext } from "react";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GitHubContext";
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { users, fetchUsers } = useContext(GithubContext);
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-    const data = await response.json();
-    setUsers(data);
-  };
-
   const showUsers = () => {
-      return users.map((user, index) => <UserItem key={index} user={user} />);
-    };
-    
+    return users.map((user, index) => <UserItem key={index} user={user} />);
+  };
 
   const showLoading = () => {
     return <Spinner />;
   };
 
-  return <div className="users_container">{users.length ? showUsers() : showLoading()}</div>;
+  return (
+    <div className="users_container">
+      {users.length ? showUsers() : showLoading()}
+    </div>
+  );
 }
 
 export default UserResults;
